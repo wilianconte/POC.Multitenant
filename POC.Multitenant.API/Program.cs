07e88ccedadd7f0beb19060faf156ee130857838
Dirceu Wilian de Camargo Conte <wilianconte@gmail.com>
2024-01-10 11:50:56 -0300

    Update Program.cs
    Fix

--- POC.Multitenant.API/Program.cs
+++ POC.Multitenant.API/Program.cs
@@ -6,7 +6,6 @@ using POC.Multitenant.Domain.Middleware;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
